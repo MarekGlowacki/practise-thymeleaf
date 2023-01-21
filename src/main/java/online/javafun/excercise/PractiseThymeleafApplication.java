74c0feb95d48960df465da3d249eb89b650b87aa
User <user@its.me>
2023-01-21 08:28:23 +0100

    Initial commit

---
+++ src/main/java/online/javafun/excercise/PractiseThymeleafApplication.java
@@ -0,0 +1,13 @@
+package online.javafun.excercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PractiseThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PractiseThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/javafun/excercise/PractiseThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package online.javafun.excercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PractiseThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
